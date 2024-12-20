2579db27d438f0bba5080d204c04d52968d56614
KratikMujawdiya <mujawdiyakratik2901@gmail.com>
2024-12-20 23:55:43 +0530

    update

---
+++ src/Components/Tours.js
@@ -0,0 +1,29 @@
+import Card from "./Card";
+
+function Tours({ tours, removeTour }) {
+  return (
+    <div className="container">
+      {/* heading */}
+      <div>
+        <h2 className="title">Plan With Love</h2>
+      </div>
+      {/* content */}
+      <div className="cards">
+
+        {/* here instead of writing different cards for different cities in tours array we use map function */}
+        {/* key={tour.id} => it is a good practice to pass key while using map function And agar koi bhi id unique nhi hai  to tour.id ki jagah index pass kar sakte hai */}
+      
+        {
+          tours.map((tour) => {
+          return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
+          })
+        }
+
+      </div>
+    </div>
+  );
+}
+
+export default Tours;
+
+// ...tour se mene pure ke pure tour object ki copy pass kar di
